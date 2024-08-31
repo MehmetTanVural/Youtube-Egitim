b5deb2638703e6720c2b18d6995af3ded03c624c
Mehmet Tan VURAL <87650561+MehmetTanVural@users.noreply.github.com>
2024-08-31 09:10:27 +0300

    Add files via upload

---
+++ SolidPrinciple/SolidPrinciple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+        }
+
+    }
+
+    //Single Responsibility
+
+    class User
+    {
+ 
+        void GetUserInfo(string name) { }
+    }
+
+}
+
+class Write()
+{
+    void Add(string name) { }
+    void Update(string name) { }
+}
+
+class Read()
+{
+    void GetAll(string name) { }
+    void GetAllByCategoryId(int id) { }
+}
